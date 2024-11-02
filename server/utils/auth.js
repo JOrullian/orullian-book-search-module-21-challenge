672d2d3d8fed76be4f420a89c2dbd7ab4e3f8247
JOrullian <jedediahorullian@gmail.com>
2024-11-02 15:39:37 -0500

    added typeDefs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,42 @@
+const { AuthenticationError } = require('apollo-server-express');
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
+  // Function to sign a token for a user
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
+  // Function to verify the token and add user data to the context
+  authMiddleware: function ({ req }) {
+    // Extract token from request headers
+    let token = req.headers.authorization;
+
+    // Check if the token is provided
+    if (token) {
+      // Format the token if it starts with "Bearer"
+      token = token.split(' ').pop().trim();
+    } else {
+      // If no token, return the request unchanged
+      return req;
+    }
+
+    try {
+      // Verify token and get user data out of it
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      // Attach user data to the request object
+      req.user = data;
+    } catch (error) {
+      console.log('Invalid token');
+      throw new AuthenticationError('Invalid token!');
+    }
+
+    // Return the request with user data attached
+    return req;
   },
 };
